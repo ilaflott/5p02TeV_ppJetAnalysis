c1ca979a37b489704709b782559bba3f04e7d816
ilaflott <ilaflott@gmail.com>
2016-05-25 17:05:25 -0400

    empty

--- PP_MC/filelists/makeMC_filelists.sh
+++ PP_MC/filelists/makeMC_filelists.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# unmerged files
-# comment: pp reconstruction, jec v12, PrivMC
+## Official MC, pp MinBias
+ls /mnt/hadoop/cms/store/user/rbi/merged/MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2/0.root >> 5p02TeV_Py8_CUETP8M1_MinBias_HINppWinter16_merged_forests.txt
+##
+
+## pp Reco Dijet MC Forests
+# pp reconstruction, jec v12, unmerged files
+## pthat bins {15,30,50,80,120,170,220,280,370,460,540}
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet15_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet15_forests.txt
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet30_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet30_forests.txt
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet50_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet50_forests.txt
@@ -13,15 +18,11 @@ ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020G
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet370_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet370_forests.txt
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet460_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet460_forests.txt
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet540_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjet540_forests.txt
-# this list is a conglomerate of all above lists
+## combined list, pthat bins {15,30,50,80,120,170,220,280,370,460,540}
 ls /mnt/hadoop/cms/store/user/krajczar/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV/Pythia8_Dijet*_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/*/0000/HiForestAOD_*.root >> 5p02TeV_Py8_CUETP8M1_QCDjetAllPtBins_forests.txt
-##
-
-# same events as QCDJet???/QCDjetAllPtBins as above, but all file of specific pthat merged into one file
-# comment: HcalRespCorr_v4
+## merged forests, same events at above files, HcalRespCorr_v4
 ls /mnt/hadoop/cms/store/user/abaty/mergedForests/PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV_ppSignal/Pythia8_Dijet*_pp_TuneCUETP8M1_5020GeV_FOREST_758_PrivMC/0.root >> 5p02TeV_Py8_CUETP8M1_QCDjetAllPtBins_merged_forests.txt
 ##
 
-# Official MinBias, HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2
-ls /mnt/hadoop/cms/store/user/rbi/merged/MinBias_TuneCUETP8M1_5p02TeV-pythia8-HINppWinter16DR-NoPU_75X_mcRun2_asymptotic_ppAt5TeV_forest_v2/0.root >> 5p02TeV_Py8_CUETP8M1_MinBias_HINppWinter16_merged_forests.txt
-##
+# pp dijetMC w/ HI reco available, see 2015 HiForest twiki if interested
+# pp dijetMC+MinBias w/ pp reco available for pileup study, see 2015 HiForest twiki if interested

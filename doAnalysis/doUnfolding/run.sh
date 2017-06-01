7484e29ff9929e8ad08d42f0f00e63fd88133471
ilaflott <ilaflott@gmail.com>
2017-06-01 12:57:33 -0400

    new code added

---
+++ doAnalysis/doUnfolding/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#echo ""
+#echo "compiling unfoldDataSpectra"
+#echo ""
+#rooUnfoldCompile unfoldDataSpectra.C
+#
+#echo ""
+#echo "done compiling unfoldDataSpectra"
+#echo ""
+#sleep 0.5s
+#
+## R = 4
+#source run_unfoldDataSpectra.sh 4 0 0 9 1
+#sleep 0.5s
+#source run_unfoldDataSpectra.sh 4 1 0 9 1
+#sleep 0.5s
+#source run_unfoldDataSpectra.sh 4 0 0 10 2
+#sleep 0.5s
+#source run_unfoldDataSpectra.sh 4 1 0 10 2
+#sleep 0.5s
+
+
+
+
+echo ""
+echo "compiling doMCClosureTests"
+echo ""
+rooUnfoldCompile doMCClosureTests.C
+
+echo ""
+echo "done compiling doMCClosureTests"
+echo ""
+sleep 0.5s
+
+# R = 4
+source run_doMCClosureTests.sh 4 0 0 9 1
+sleep 0.5s
+source run_doMCClosureTests.sh 4 1 0 9 1
+sleep 0.5s
+source run_doMCClosureTests.sh 4 0 0 10 2
+sleep 0.5s
+source run_doMCClosureTests.sh 4 1 0 10 2
+sleep 0.5s
---
+++ doAnalysis/doUnfolding/run_unfoldDataSpectra.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+
+##CONST
+date_data="05-25-17"
+date_condor="5.25.17"
+outCondorDir="${date_condor}_outputCondor"
+##CONST
+
+
+if [ $# -ne 5 ]
+then
+    echo "bad usage."
+    echo "source run_unfoldSpectra <R> <jtID> <compile> <SVDkReg> <etaBin#>"
+    echo ""
+
+    echo "e.g."
+    echo "source run_unfoldSpectra 3 1 1 20 1"
+    echo ""
+
+    return
+fi
+
+R=$1
+jtID=$2
+compile=$3
+kReg=$4
+etaBin=$5
+
+if [ $compile -eq 1 ]
+then
+    echo ""
+    echo "compiling unfoldDataSpectra"
+    echo ""
+    rooUnfoldCompile unfoldDataSpectra.C
+
+    echo ""
+    echo "done compiling unfoldDataSpectra"
+    echo ""
+    sleep 0.5s
+else
+    echo ""
+    echo "skipping compilation..."
+    echo ""
+fi
+
+
+
+
+#### unfoldDataSpectra
+echo ""
+echo "running unfoldDataSpectra"
+echo ""
+
+if [ $etaBin -eq 1 ]
+then
+    ./unfoldDataSpectra.exe ${outCondorDir}/ppData_HighPtJetTrig_ak${R}PFJets_${date_data}_jetPlots_0.0eta0.5  ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.0eta0.5     HPtJetTrig_Py8_unfData_00eta05   ${jtID} ${kReg}
+elif [ $etaBin -eq 2 ]
+then
+    ./unfoldDataSpectra.exe ${outCondorDir}/ppData_HighPtJetTrig_ak${R}PFJets_${date_data}_jetPlots_0.5eta1.0  ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.5eta1.0     HPtJetTrig_Py8_unfData_05eta10   ${jtID} ${kReg}
+else
+    echo ""
+    echo "etaBin not found; exit"
+    echo ""
+    return
+fi
+
+#./unfoldDataSpectra.exe ${outCondorDir}/ppData_HighPtJetTrig_ak${R}PFJets_${date_data}_jetPlots_1.0eta1.5  ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.0eta1.5     HPtJetTrig_Py8_unfData_10eta15  ${jtID}
+#./unfoldDataSpectra.exe ${outCondorDir}/ppData_HighPtJetTrig_ak${R}PFJets_${date_data}_jetPlots_1.5eta2.0  ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.5eta2.0     HPtJetTrig_Py8_unfData_15eta20  ${jtID}
+
+
+
+
+#echo ""
+#echo "done running unfoldDataSpectra"
+#echo ""
+#sleep 0.5s
+#
+#
+#
+#
+#
+##### doMCClosureTests
+##echo ""
+##echo "compiling doMCClosureTests"
+##echo ""
+##rooUnfoldCompile doMCClosureTests.C
+##
+##echo ""
+##echo "done compiling doMCClosureTests"
+##echo ""
+##sleep 0.5s
+##
+###echo ""
+###echo "running doMCClosureTests"
+###echo ""
+##
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.0eta0.5     Py8_closureTest_00eta05_0jtID  0
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.5eta1.0     Py8_closureTest_05eta10_0jtID  0
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.0eta1.5     Py8_closureTest_10eta15_0jtID  0
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.5eta2.0     Py8_closureTest_15eta20_0jtID  0
+#
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.0eta0.5     Py8_closureTest_00eta05_wjtID  1
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_0.5eta1.0     Py8_closureTest_05eta10_wjtID  1
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.0eta1.5     Py8_closureTest_10eta15_wjtID  1
+###./doMCClosureTests.exe   ${outCondorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_data}_JERS_1.5eta2.0     Py8_closureTest_15eta20_wjtID  1
+
+
+echo ""
+echo "done!"
+echo ""
+
+return

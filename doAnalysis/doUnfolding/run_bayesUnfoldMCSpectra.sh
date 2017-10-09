2cb28dc39267e7b224b8a63cf320c4c9a468b0bf
ian laflotte <ilaflott@gmail.com>
2017-10-09 14:21:24 -0400

    added new scripts

---
+++ doAnalysis/doUnfolding/run_bayesUnfoldMCSpectra.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+##CONST
+jtID=1
+##CONST
+
+R=$1
+condorDir=$2
+date_output=$3
+etaBin=$4
+etaBinOut=$5
+kReg=$6
+useSimpleBinning=$7
+
+
+
+#### unfoldMCSpectra
+echo ""
+echo "running unfoldMCSpectra"
+echo ""
+
+./bayesUnfoldMCSpectra.exe ${condorDir}/ppMC_Py8_CUETP8M1_QCDjetAllPtBins_ak${R}PFJets_${date_output}_JERS_${etaBin}  Py8_closureTest_${etaBinOut} ${jtID} ${kReg} ${useSimpleBinning}
+
+return

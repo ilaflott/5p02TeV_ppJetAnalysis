9ed7430cc9238d7c35050ca3aa81606964671079
ilaflott <ilaflott@gmail.com>
2018-06-21 16:02:33 -0400

    added new files

---
+++ doAnalysis/doUnfolding/unfoldSpectra_thySpectra.h
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <cstring>
+
+
+std::string fNLOJetsSpectra_dir="/home/ilaflott/5p02TeV_ppJetAnalysis/CMSSW_7_5_8/src/doAnalysis/doUnfolding/smearTheory/fNLOJetsSpectra";
+
+//std::string fNLO_filename_start="fnl5020_LO2_R0";
+//std::string fNLO_filename_mid="Jets_modify_";
+
+std::string fNLOFile_R04_CT10nlo  	  = fNLOJetsSpectra_dir+"/R04/fnl5020_LO2_R04Jets_modify_CT10nlo_HS.root";
+std::string fNLOFile_R04_CT14nlo  	  = fNLOJetsSpectra_dir+"/R04/fnl5020_LO2_R04Jets_modify_CT14nlo_HS.root";
+std::string fNLOFile_R04_HERAPDF  	  = fNLOJetsSpectra_dir+"/R04/fnl5020_LO2_R04Jets_modify_HERAPDF15NLO_ALPHAS_HS.root";
+std::string fNLOFile_R04_MMHTnlo  	  = fNLOJetsSpectra_dir+"/R04/fnl5020_LO2_R04Jets_modify_MMHT2014nlo68cl_HS.root";
+std::string fNLOFile_R04_NNPDFnnlo        = fNLOJetsSpectra_dir+"/R04/fnl5020_LO2_R04Jets_modify_NNPDF30_nnlo_as_0121_MC.root";
+
+
+
+
